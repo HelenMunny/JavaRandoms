64eb2ddddac57b098d88c164b5d70a8cf266a1ab
HelenMunny <66639204+HelenMunny@users.noreply.github.com>
2023-09-21 11:26:51 -0400

    java random practices

---
+++ javaWarmUp/src/sep14/practice.java
@@ -0,0 +1,10 @@
+package sep14;
+
+public class practice {
+
+    public static void main(String[] args){
+        System.out.println("Hello world!");
+    }
+
+
+}
---
+++ javaWarmUp/src/sep16/practice2.java
@@ -0,0 +1,57 @@
+package sep16;
+
+public class practice2 {
+    public static void main(String[] args){
+
+//        QUESTION-1: PRINT SUARES OF 5 TO 15
+        for(int i=5; i<=15; i++){
+            System.out.println(i*i);
+        }
+        System.out.println("======================================");
+
+
+//        QUESTION-2: CALCULATE THE SUM OF ALL NUMBERA FROM -5 TO 18
+        int sum=0;
+        for(int i=-5; i<=18; i++){
+            sum+=i;
+        }
+        System.out.println(sum);
+
+        System.out.println("======================================");
+
+//        QUESTION-3: WRITE A PROGRAM TO PRINT A MULTIPLICATION OF 7
+        int multiple = 7;
+
+        for(int i=1;i<=10;i++){
+            System.out.println(multiple+ "*" + i + "=" + multiple*i);
+        }
+        System.out.println("======================================");
+
+//        QUESTION-4: COUNT THE TOTAL NUMBER OF DIGITS IN 67687
+        int number = 67687;
+        int length = String.valueOf(number).length();
+        System.out.println (length);
+        System.out.println("======================================");
+
+//        QUESTION-5: PRINT NUMBERS FROM -10 TO -1 USING FOR LOOP
+//        QUESTION-6: PRINT ALL ODD NUMBERS BETWEEN 1 TO 99
+//        QUESTION-7: PRINT FIBONACCI SERIES UP TO 10 TERMS
+//        QUESTION-8: CALCULATE THE SUM OF ALL NUMBERA FROM -5 TO 18
+
+
+
+//        QUESTION-9: PRINT ALL NUMBERS 342,432,765,234,264,322
+//        QUESTION-10: FIND THE LOWEST NUMBER FROM 342,-231,432, 997, 765, -715, 234, 265, 322
+//        QUESTION-11: FIND THE BIGGEST NUMBER FROM 342,-231,432, 997, 765, -715, 234, 265, 322
+//        QUESTION-12: FIND THE LOWEST NUMBER FROM 342,-231,432, 997, 765, -715, 234, 265, 322
+//        QUESTION-13: ADD ALL THE NUMBERS AND PRINT THE SUM OF 342,-231,432, 997, 765, -715, 234, 265, 322
+
+
+    }
+}
+
+
+
+
+
+
