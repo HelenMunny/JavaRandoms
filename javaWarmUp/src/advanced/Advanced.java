d7cd37ff451aaf42a8464b11e4752785f4019a95
HelenMunny <66639204+HelenMunny@users.noreply.github.com>
2023-09-25 16:30:29 -0400

    static and non static

--- javaWarmUp/src/advanced/Advanced.java
+++ javaWarmUp/src/advanced/Advanced.java
@@ -1,7 +1,21 @@
 package advanced;
 
 public class Advanced {
-    public static void main(String[] args){
-        System.out.println("Hello");
-    }
+public static void main(String[] args){
+Advanced advanced = new Advanced();
+advanced.method();
+
 }
+
+ void method(){
+    Practice3.stat("static","object creation");
+    System.out.println(Practice3.stat);
+
+    Practice3 practice3 = new Practice3();
+    practice3.nonStat("non static","object creation");
+    System.out.println(practice3.nonStat);
+}
+
+}
+
+
---
+++ javaWarmUp/src/advanced/Practice3.java
@@ -0,0 +1,13 @@
+package advanced;
+
+public class Practice3 {
+ int nonStat = 5;
+ static int stat = 6;
+
+ void nonStat(String param1, String param2){
+     System.out.println("Hello, I am a "+param1+" method. and I cannot visit a static method without "+param2);
+ }
+ static void stat (String param1, String param2){
+     System.out.println("Hello, I am a "+param1+" method. and I can visit a static/non-static method without "+param2+" but for visiting another class, I enter through the class name.");
+ }
+}
